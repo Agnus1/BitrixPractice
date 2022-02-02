c02e06d32142c0799be7d1637e93157bcd1243ae
i.kim <i.kim@qsoft.ru>
2022-02-02 17:39:01 +0000

    204327 / Почтовые уведомления - Исправлены типовые ошибки

--- local/php_interface/include/event_handlers.php
+++ local/php_interface/include/event_handlers.php
@@ -1,26 +1,24 @@
 <?php
 
-AddEventHandler("main", "OnAfterUserLogin", Array("SendUserAuth", "OnAfterUserLoginHandler"));
+AddEventHandler("main", "OnAfterUserAuthorize", Array("SendUserAuth", "OnAfterUserAuthorizeHandler"));
 
 class SendUserAuth
 {
-    // создаем обработчик события "OnAfterUserLogin"
-    function OnAfterUserLoginHandler(&$fields)
+    // создаем обработчик события "OnAfterUserAuthorizeHandler"
+    public static function OnAfterUserAuthorizeHandler(&$fields)
     {
-        // если логин успешен то
-        if($fields['USER_ID'] > 0)
-        {
-        	// получение полей для шаблона
-	    	$email = CUser::GetEmail();
-	    	$login = $fields["LOGIN"];
-	    	$date = date('Y.m.d h:i:s', time());
-	    	$inf = [
-	    		"EMAIL" => $email,
-	    		"LOGIN" => $login,
-	    		"DATE" => $date,
-	    	];
 
-        	CEvent::Send("USER_AUTHORIZED", SITE_ID, $inf);
-        }
+        // получение полей для шаблона
+        $email = $fields["user_fields"]["EMAIL"];
+        $login = $fields["user_fields"]["LOGIN"];
+        $date = date('Y.m.d H:i:s', time());
+
+        $inf = [
+            "EMAIL" => $email,
+            "LOGIN" => $login,
+            "DATE" => $date,
+        ];
+
+        CEvent::Send("USER_AUTHORIZED", SITE_ID, $inf);
     }
 }
